9eb529c04a8b4b831d9731ca6cd72cd2d613b881
Ehson5252 <oehsoni1@gmail.com>
2024-10-13 13:49:37 -0500

    fixed bag: Deleted 3 services. Resized buttons on hero section.

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -6,29 +6,29 @@ const Services_Data = [
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
+        s_name:"Develop React application",
         s_desc:"Web development is the process of building, programming..."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"04",
         s_name:"App design",
         s_desc:"Web development is the process of building, programming..."
     },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
+    //{
+    //     s_no:"02",
+    //     s_name:"Graphics design",
+    //     s_desc:"Web development is the process of building, programming..."
+    // },
+    // {
+    //     s_no:"05",
+    //     s_name:"Digital marketing",
+    //     s_desc:"Web development is the process of building, programming..."
+    // },
+    // {
+    //     s_no:"06",
+    //     s_name:"Content writing",
+    //     s_desc:"Web development is the process of building, programming..."
+    // },
 ]
  
 export default Services_Data;
