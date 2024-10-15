fdd0652b21228aedee850cc71b11687a1ab07643
Ehson5252 <oehsoni1@gmail.com>
2024-10-15 09:12:53 -0500

    Increased size for mobile size

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './footer.css'
-import footer_logo from '../../assets/footer_logo.svg'
+import footer_logo from '../../assets/my_logo2.png'
 import user_icon from '../../assets/user_icon.svg'
 
 
@@ -9,16 +9,16 @@ const Footer = () => {
     <div className='footer'>
         <div className="footer-top">
             <div className="footer-top-left">
-                <img src={footer_logo} alt="" />
-                <p>I am a frontend developer from MO, USA with 10 years of experience</p>
+                <img src={footer_logo} alt="" className='logo'/>
+                <p>I am a frontend developer from MO, USA with 2 and more years of experience</p>
             </div>
-            <div className="footer-top-right">
+            {/* <div className="footer-top-right">
                 <div className="footer-email-input">
                     <img src={user_icon} alt="" />
                     <input type="email" placeholder='Entrer your email' />
                 </div>
                 <div className="footer-subscribe">Subscribe</div>
-            </div>
+            </div> */}
         </div>
         <hr />
         <div className="footer-bottom">
